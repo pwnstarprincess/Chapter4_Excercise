b414ad41ea243e316f7dea2b534da277239237ef
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-02-22 18:03:33 -0700

    convert textbox strings to doubles

--- Chapter4_Excercise/Form1.vb
+++ Chapter4_Excercise/Form1.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form1
+    Private Sub analyzeButton_Click(sender As Object, e As EventArgs) Handles analyzeButton.Click
 
+        Dim loanAmmount, interestRate, durationMonths As String
+        Dim loan, interest, months As Double
+
+        ''Convert from string to double
+        loan = Convert.ToDouble(loanTextBox)
+        interest = Convert.ToDouble(interestRateTextBox)
+        months = Convert.ToDouble(monthsTextBox)
+
+
+
+    End Sub
 End Class
