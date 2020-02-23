62c073a56c5a0215666d41b2448fdfb8469bc426
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-02-22 18:29:28 -0700

    convert and format monthly payment and total interest paid.  Output to applicable textboxes

--- Chapter4_Excercise/Form1.vb
+++ Chapter4_Excercise/Form1.vb
@@ -13,7 +13,13 @@
         monthlyPayment = (loanPrincipal * interest) / (1 - (1 + interest) ^ months)
         totalInterest = months * monthlyPayment - principal
 
+        ''Convert monthly payment and total interest to string and format
+        paymentPerMonth = monthlyPayment.ToString("0.00")
+        interestTotal = totalInterest.ToString("0.00")
 
+        ''Output and format monthly payment and total interest to applicable textboxes
+        monthlyPaymentTextBox.AppendText("$" + paymentPerMonth)
+        totalInterestTextBox.AppendText("$" + interestTotal)
 
 
     End Sub
