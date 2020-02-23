27029796ab9cb17f7cf8c3c468e4998b18d21640
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-02-22 18:49:45 -0700

    fix monthly payment calculation

--- Chapter4_Excercise/Form1.vb
+++ Chapter4_Excercise/Form1.vb
@@ -6,20 +6,20 @@
 
         ''Convert from string to double
         principal = Convert.ToDouble(principalTextBox.Text)
-        interest = Convert.ToDouble(interestRateTextBox.Text)
+        interest = Convert.ToDouble(interestRateTextBox.Text) / 100
         months = Convert.ToDouble(monthsTextBox.Text)
 
         ''Calculate monthly payment and total interest
-        monthlyPayment = (principal * interest) / (1 - (1 + interest) ^ months)
+        monthlyPayment = (principal * (interest / 12)) / (1 - (1 + (interest / 12)) ^ (-months))
         totalInterest = months * monthlyPayment - principal
 
         ''Convert monthly payment and total interest to string and format
-        paymentPerMonth = monthlyPayment.ToString("0.00")
-        interestTotal = totalInterest.ToString("0.00")
+        paymentPerMonth = monthlyPayment.ToString("$0,00.00")
+        interestTotal = totalInterest.ToString("$0,00.00")
 
         ''Output and format monthly payment and total interest to applicable textboxes
-        monthlyPaymentTextBox.AppendText("$" + paymentPerMonth)
-        totalInterestTextBox.AppendText("$" + interestTotal)
+        monthlyPaymentTextBox.AppendText(paymentPerMonth)
+        totalInterestTextBox.AppendText(interestTotal)
 
 
     End Sub
